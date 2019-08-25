83a3bbc14514b22d7246d9a4165a81a0c34e5752
Adedamola Elusakin <adedamola.elusakin@gmail.com>
2019-08-25 14:15:21 +0100

    made modifications for edge cases

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let evaluation = "";
 calculator.answer.value = "";
-var acceptableInput = ["1","2","3","4","5","6","7","8","9","0","-","+","/","*","="]
+var acceptableInput = ["1","2","3","4","5","6","7","8","9","0","+","-","/","*"]
 
 function handleNumberClick(value){
     evaluation = evaluation + value;
@@ -8,8 +8,23 @@ function handleNumberClick(value){
 }
 
 function handleOperatorClick(value){
-    evaluation = evaluation + value;
-    calculator.answer.value = evaluation;
+    var currentExpression = calculator.answer.value;
+    if(value == "/" || value == "*"){
+      //check that it is not the first entry
+      if(currentExpression.length == 0){
+        return;
+      }
+    }
+
+    //check that two operators do not follow
+    var lastChar = currentExpression[currentExpression.length - 1];
+    //alert(lastChar)
+    if(lastChar != null){
+      if(!isNaN(lastChar)){
+        evaluation = evaluation + value;
+        calculator.answer.value = evaluation;
+      }
+    }
 }
 
 function clear(){
@@ -18,6 +33,12 @@ function clear(){
 }
 
 function compute(){
+    var lastChar = evaluation[evaluation.length - 1];
+    if(isNaN(lastChar)){
+      evaluation = evaluation = evaluation.slice(0,-1);
+      calculator.answer.value = evaluation;
+    }
+    
     let result = eval(evaluation);
     evaluation = result;
     calculator.answer.value = evaluation;;
@@ -47,5 +68,21 @@ document.getElementById("answer").addEventListener("input", function(event){
    if(!acceptableInput.includes(lastChar)){
      evaluation = evaluation.slice(0,-1);
      calculator.answer.value = evaluation;
+     return;
    } 
+
+   evaluation = evaluation.slice(0,-1);
+   calculator.answer.value = evaluation;
+ 
+   if(lastChar == "="){
+     compute();
+   }
+   
+   if(isNaN(lastChar) == false){
+     handleNumberClick(lastChar);
+   }
+   else{
+     handleOperatorClick(lastChar);
+   }
+   
 });
